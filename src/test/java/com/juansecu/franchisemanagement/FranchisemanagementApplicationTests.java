d7f17c4ee26f149234ed4d1ee1e4495634a09ea8
Juansecu <43616657+Juansecu@users.noreply.github.com>
2026-04-13 23:04:31 -0500

    Initial commit

---
+++ src/test/java/com/juansecu/franchisemanagement/FranchisemanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juansecu.franchisemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FranchisemanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
