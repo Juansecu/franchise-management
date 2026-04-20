315c8eb71bff46279a931a4d4f6b289abbd4f2be
Juansecu <43616657+Juansecu@users.noreply.github.com>
2026-04-20 04:45:49 -0500

    CONFIG - Unify local and prod configurations

--- infrastructure/templates/render/create-web-service.tf
+++ infrastructure/templates/render/create-web-service.tf
@@ -11,8 +11,11 @@ resource "render_web_service" "web" {
   }
 
   env_vars = {
-    "POSTGRES_DATABASE_URL" = { value = "jdbc:${render_postgres.postgres_instance.connection_info.internal_connection_string}" },
-    "SPRING_PROFILES_ACTIVE" = { value = "prod" }
+    "POSTGRES_DATABASE_NAME" = { value = local.postgres_database_name },
+    "POSTGRES_HOST" = { value = local.postgres_host },
+    "POSTGRES_PASSWORD" = { value = local.postgres_password },
+    "POSTGRES_PORT" = { value = "5432" },
+    "POSTGRES_USER" = { value = local.postgres_user }
   }
 
   notification_override = {
@@ -23,4 +26,28 @@ resource "render_web_service" "web" {
   log_stream_override = {
     setting = "drop"
   }
+}
+
+locals {
+  postgres_connection_string = render_postgres.postgres_instance.connection_info.internal_connection_string
+
+  postgres_database_name = regex(
+    "^postgresql://[^:]+:[^@]+@[^/]+/([^?]+)$",
+    local.postgres_connection_string
+  )[0]
+
+  postgres_host = regex(
+    "^postgresql://[^:]+:[^@]+@([^/]+)/",
+    local.postgres_connection_string
+  )[0]
+
+  postgres_password = regex(
+    "^postgresql://[^:]+:([^@]+)@",
+    local.postgres_connection_string
+  )[0]
+
+  postgres_user = regex(
+    "^postgresql://([^:]+):[^@]+@",
+    local.postgres_connection_string
+  )[0]
 }
